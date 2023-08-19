f1d99f8ba80d1b1ce1bc247b50aeec16a46f7915
571979 <jmejia@impulse-telecom.com>
2023-08-18 18:17:11 -0600

    termino seccion 7

--- src/SegundoComponente.js
+++ src/SegundoComponente.js
@@ -2,19 +2,21 @@ import React from 'react'
 
  const SegundoComponente = () => {
 
-    const libros = ['hp','gt','batman'];
-
+    //const libros = ['hp','gt','batman'];
+   const libros = [];
   return (
     <div className="segundo-componente">
         <h1>listado de libros</h1>
-        <ul>
+        {libros.length >= 1 ? 
+        (<ul>
             {
                 libros.map((libro, indice) => {
                 return <li key={indice}>{libro}</li>;
                 })
             }
-            </ul>
-
+            </ul>)
+            :(<p>no hay libros</p>)
+        }
     </div>
   )
 }
